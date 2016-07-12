69c55cdf1294a9be0cdb4fc363845120efedd492
Bob Dill <rddill@us.ibm.com>
2016-07-12 18:34:47 -0500

    find inserted

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -5,5 +5,5 @@ GCC_VERSION="4.9"
 export R_HOME=/app/vendor/R
 export R_INCLUDE=$R_HOME/lib64/R/include
 export PATH=$R_HOME/bin:/app/vendor/gcc/bin:$PATH
-export LD_LIBRARY_PATH="/lib:/usr/lib:$R_HOME/lib64:$LD_LIBRARY_PATH"
-export LIBRARY_PATH="/lib:/usr/lib:$R_HOME/lib64:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH=".$R_HOME/lib:/lib:/usr/lib:$LD_LIBRARY_PATH"
+export LIBRARY_PATH="$R_HOME/lib:/lib:/usr/lib:$LD_LIBRARY_PATH"
