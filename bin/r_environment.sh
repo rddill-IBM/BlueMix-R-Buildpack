eac5516bc207710d728ecc1a1944795be479bf51
Bob Dill <rddill@us.ibm.com>
2016-07-12 22:48:58 -0500

    working on path

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -6,4 +6,4 @@ export R_HOME=/app/vendor/R
 export R_INCLUDE=$R_HOME/lib64/R/include
 export PATH=$R_HOME/bin:/app/vendor/gcc/bin:$PATH
 export LD_LIBRARY_PATH=".$R_HOME/lib:/lib:/usr/lib:$LD_LIBRARY_PATH"
-export LIBRARY_PATH="$R_HOME/lib:/lib:/usr/lib:$LD_LIBRARY_PATH"
+export LIBRARY_PATH=".$R_HOME/lib:/lib:/usr/lib:$LD_LIBRARY_PATH"
