e55855e4b287d23602f4a99fe195184052cc9d5e
Bob Dill <rddill@us.ibm.com>
2016-08-31 15:28:49 -0400

    tested compile 2016 08 31

--- bin/r_environment.sh
+++ bin/r_environment.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-GCC_VERSION="4.9"
-
-export R_HOME=/app/vendor/R
-export R_INCLUDE=$R_HOME/lib64/R/include
-export PATH=$R_HOME/bin:/app/vendor/gcc/bin:$PATH
-export LD_LIBRARY_PATH=".$R_HOME/lib:/lib:/usr/lib:$LD_LIBRARY_PATH"
-export LIBRARY_PATH=".$R_HOME/lib:/lib:/usr/lib:$LD_LIBRARY_PATH"
+# export R_HOME=$BUILD_DIR/usr/lib/R
+# export R_INCLUDE=/$BUILD_DIR/usr/share/R/include
+# export PATH=$R_HOME/bin:/app/usr/lib/gcc:$PATH
+# export LD_LIBRARY_PATH="/lib:/usr/lib:usr/lib/R/library:$LD_LIBRARY_PATH"
+# export LIBRARY_PATH="/lib:/usr/lib:usr/lib/R/library:$LD_LIBRARY_PATH"
